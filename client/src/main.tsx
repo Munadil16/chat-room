849a80e8a7ea1c2939431469585d913e708ebcd6
Munadil16 <munadilxd@gmail.com>
2024-12-14 16:09:01 +0530

    chore: Add shadcn/ui to client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,15 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import "./index.css";
-import App from "./App.tsx";
+import "@/index.css";
+import App from "@/App";
+import { ThemeProvider } from "@/components/theme-provider";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<App />}></Route>
-            </Routes>
-        </BrowserRouter>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<App />}></Route>
+                </Routes>
+            </BrowserRouter>
+        </ThemeProvider>
     </StrictMode>
 );
